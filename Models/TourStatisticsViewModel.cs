8e01e613a11736dea40c0059ab1e6206001a43fa
MintPhut <nguyenminhphuc06102019@gmail.com>
2025-08-01 10:56:11 +0700

    capnhat

---
+++ Models/TourStatisticsViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace test4.Models
+{
+    public class TourStatisticsViewModel
+    {
+        public int TourID { get; set; }
+        public string TourName { get; set; } = null!;
+        public int NumberOfDestinations { get; set; }
+        public int TotalTourDays { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
